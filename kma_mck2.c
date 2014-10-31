2f20ad3393698f4f8a57c3ab1de515536436a124
jameswhang <sungyoonwhang2017@u.northwestern.edu>
2014-10-30 20:05:17 -0500

    added code for malloc

--- kma_mck2.c
+++ kma_mck2.c
@@ -54,25 +54,51 @@
 typedef struct 
 {
   void * nextblock;
-} buffer_header;
+} buffer_t;
+
+typedef struct
+{
+  buffer_t* header;
+} buffer_list_t;
 
 typedef struct page_node_struct
 {
   kma_page_t* page;
   struct page_node_struct* next; 
-} page_node;
+} page_node_t;
 
-typedef struct
+typedef struct pagehead
 {
-  int size; // size of buffers in this free list
-  int used; // number of blocks used
-  buffer_header* start; // pointer to the first buffer in the linked list
-  page_node* page_list;
-} free_list;
+  int num_alloc;
+  int size;
+  void* ptr;
+  void* next_block;
+  struct pagehead* next_page; 
+} page_header_t; 
+
+typedef struct 
+{
+  page_header_t header;
+  int page_num;
+  buffer_list_t freelist[10];
+  page_header_t* header;
+} mainpage_t;
 
 /************Global Variables*********************************************/
-
+kma_page_t* entrypage;
 /************Function Prototypes******************************************/
+kma_page_t* init_entrypage(kma_page_t* page);
+kma_page_t* init_page(kma_page_t* page, kma_size_t size); 
+
+void* search_bins(kma_size_t size); 
+void* search_pages(kma_size_t size);
+
+void remove_allocation(void* ptr);
+void clear_bins(kma_page_t* page);
+
+int page_number(long val, long fp_header);
+size_t hash(size_t size); 
+kma_size_t blk_size(kma_size_t size);
 
 /************External Declaration*****************************************/
 
@@ -81,7 +107,46 @@ typedef struct
 void*
 kma_malloc(kma_size_t size)
 {
-  return NULL;
+  if((size+sizeof(kma_page_t*)) > PAGESIZE) {
+    return NULL;
+  }
+  if(entrypage == NULL) {
+    // initialize the entry page
+    entrypage = init_entrypage(get_page());
+  }
+
+  // Entry page header set 
+  mainpage_t* entrypage_header = (mainpage_t*)(entrypage->ptr);
+  kma_size_t alloc_size = blksize(size);
+
+  // Search through the bins 
+  void* alloc = search_bins(size); 
+
+  if(alloc != NULL) {
+    // Bin search was successful
+    int page = page_number((long)alloc, (long)entrypage);
+    page_header_t* current = (page_header_t*)((long)entrypage_header + (page * PAGESIZE));
+  
+    // Increment Allocations
+    current->num_alloc++;
+    entrypage_header->header.num_alloc++;
+    alloc_remove(alloc);
+    return alloc;
+  } else {
+    // Bin search was unsuccessful 
+    alloc = search_pages(size);
+    if(alloc != NULL) {
+      // search page successful
+      int page = page_number((long)alloc, (long)entrypage_header); 
+      page_header_t* current = (page_header_t*)((long)entrypage_header + (page * PAGESIZE));
+
+      // Increment allocations
+      current->num_alloc++;
+      entrypage_header->header.num_alloc++;
+
+      current->nextblock = (void*)((long)current->next+block + alloc_size);
+      
+  }
 }
 
 void
